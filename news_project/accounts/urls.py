d7f89e675537f6bf7f685ce03eea4273c6caa826
Azizbek Murodov <azizbekmurodov2003@gmail.com>
2023-09-22 00:11:12 +0500

    rename user password

--- news_project/accounts/urls.py
+++ news_project/accounts/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import user_login,dashboard_view
-from django.contrib.auth.views import LoginView,LogoutView
+from django.contrib.auth.views import LoginView,LogoutView,PasswordChangeView,PasswordChangeDoneView
 
 
 urlpatterns = [
    # path('login/', user_login, name='login'),
     path('login/',LoginView.as_view(), name='login'),
     path('logout/',LogoutView.as_view(), name='logout'),
+    path('password-change/',PasswordChangeView.as_view(), name='password_change'),
+    path('password-change-done/',PasswordChangeDoneView.as_view(),name='password_change_done'),
     path('profile/', dashboard_view, name='user_profile'),
 ]
