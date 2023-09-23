6fba9fe01958b78a8951ee2a2faf88e2486ab643
Azizbek Murodov <azizbekmurodov2003@gmail.com>
2023-09-24 00:14:14 +0500

    added reset password codes

--- news_project/accounts/urls.py
+++ news_project/accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import user_login,dashboard_view
-from django.contrib.auth.views import LoginView,LogoutView,PasswordChangeView,PasswordChangeDoneView
+from django.contrib.auth.views import LoginView,LogoutView,PasswordChangeView, \
+    PasswordChangeDoneView,PasswordResetView,PasswordResetConfirmView,PasswordResetDoneView,PasswordResetCompleteView
 
 
 urlpatterns = [
@@ -9,5 +10,10 @@ urlpatterns = [
     path('logout/',LogoutView.as_view(), name='logout'),
     path('password-change/',PasswordChangeView.as_view(), name='password_change'),
     path('password-change-done/',PasswordChangeDoneView.as_view(),name='password_change_done'),
+    path('password-reset/',PasswordResetView.as_view(),name='password_reset'),
+    path('password-reset/done/',PasswordResetDoneView.as_view(),name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>',PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
+    path('password-reset/complete/', PasswordResetCompleteView.as_view() , name='password_reset_complete'),
+
     path('profile/', dashboard_view, name='user_profile'),
 ]
